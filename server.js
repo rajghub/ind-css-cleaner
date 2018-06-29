5b3e98506e1a70a285f419865346b89a7636f185
Rajesh <rajsv456@gmail.com>
2018-06-29 16:52:29 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+app.use(express.static(__dirname + '/public'));
+app.listen(process.env.PORT || 4000);
+module.exports = {app}
