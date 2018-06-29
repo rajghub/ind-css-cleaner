6836e90e9053385826776b856c27f4f7cfd40ae9
Rajesh <rajsv456@gmail.com>
2018-06-29 17:37:40 +0530

    First commit

--- server.js
+++ server.js
@@ -16,5 +16,37 @@ var request = require('request');
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 app.use(express.static(__dirname + '/public'));
-app.listen(process.env.PORT || 4000);
-module.exports = {app}
+
+
+
+
+app.post('/test', function(req, res){
+	//console.log('body: ' + req.body.name);
+	var mergeHTML = [];
+	req.body.name.forEach(function(arr,index){
+		request(arr, function (error, response, body) {
+		  var $ = cheerio.load(body.toString());
+		  console.log($("title").toString())
+		  mergeHTML.push($("body").toString());
+
+			if(req.body.name.length == mergeHTML.length){
+				res.json({"name":mergeHTML});
+			}
+		  
+		});
+		
+		
+	})
+
+	
+	
+	
+	
+	
+	
+	
+});
+
+app.listen(5555, function() {
+    console.log("ready")
+})
